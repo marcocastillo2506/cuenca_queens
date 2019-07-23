8406a53b631bdf0f0c81ac60aefa42f2119c8f44
marcocastillo2506 <marcocastillo2506@gmail.com>
2019-07-23 04:09:03 -0500

    implementacion de base de datos(postgresql) usando sqlalchemy

---
+++ db.py
@@ -0,0 +1,28 @@
+from pyqueens import size, solucion_individual, solutions
+from sqlalchemy import create_engine  
+from sqlalchemy import Column, Integer, ARRAY
+from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.orm import sessionmaker
+
+db_string = "postgres://postgres:Holacode@localhost:5432/soluciones"
+
+db = create_engine(db_string)  
+base = declarative_base()
+
+class NQueens(base):  
+    __tablename__ = 'reynas'
+
+    numero_de_reynas = Column(Integer, primary_key=True)
+    lista = Column(ARRAY(Integer))
+    total_de_soluciones = Column(Integer)
+
+Session = sessionmaker(db)  
+session = Session()
+
+base.metadata.create_all(db)
+
+# Create 
+eight_queens = NQueens(numero_de_reynas=size, lista=solucion_individual, total_de_soluciones=solutions)  
+session.add(eight_queens)  
+session.commit()
+
